408064c5c2eb6d429283924ee320ba454e9476c5
GuzevOleg <109841909+GuzevOleg@users.noreply.github.com>
2024-03-19 20:27:40 +0400

    Update RandomUserTests.java

--- src/test/java/tests/RandomUserTests.java
+++ src/test/java/tests/RandomUserTests.java
@@ -7,15 +7,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import pojo.randomUser.RandomUsersResult;
 import pojo.randomUser.Result;
 
 import java.util.List;
-import java.util.stream.Stream;
 
-import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class RandomUserTests {
 
